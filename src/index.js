406d85084ece5b3d82918221a8c23815d235dcb4
Axel Diego Chacón Pérez <axel.chacon@pucp.edu.pe>
2024-09-26 18:58:38 -0500

    usando los módulos y que escuche el puerto 4000

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Soy Juancho");
+import express from "express";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
